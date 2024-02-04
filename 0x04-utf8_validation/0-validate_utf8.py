c3a4af1c589993af1891c32486a4b96328ea675c
0marSalah <omar.salah1597@gmail.com>
2024-02-04 23:22:00 +0200

    Fix bug in login functionality

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    anything
+    """
+    bytes_ = 0
+    for i in data:
+        byte = format(i, '#010b')[-8:]
+        if bytes_ == 0:
+            if byte[0] == '1':
+                bytes_ = len(byte.split('0')[0])
+            if bytes_ > 4 or bytes_ == 1:
+                return False
+            if bytes_ == 0:
+                continue
+        else:
+            if not (byte[0] == '1' and byte[1] == '0'):
+                return False
+        bytes_ -= 1
+    return bytes_ == 0
